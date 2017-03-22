b16c5917cc14fbc2cd988e077af074207b46bc58
Mohamed Ibrahim <mohamed_ibrahim15@hotmail.com>
2017-03-22 23:55:39 +0200

    prograss / disable btn / launcher / name

--- app/src/free/java/com.udacity.gradle.builditbigger.free/MainFragment.java
+++ app/src/free/java/com.udacity.gradle.builditbigger.free/MainFragment.java
@@ -6,9 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.mohamedibrahim.displayjokerlib.JokerActivity;
 import com.udacity.gradle.builditbigger.JokeAsyncTask;
 import com.udacity.gradle.builditbigger.R;
@@ -26,6 +30,12 @@ public class MainFragment extends Fragment {
 
     @BindView(R.id.adView)
     AdView mAdView;
+    @BindView(R.id.progress)
+    ProgressBar progress;
+    @BindView(R.id.btn_tell_joke)
+    Button btnTellJoke;
+
+    InterstitialAd mInterstitialAd;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,19 +48,58 @@ public class MainFragment extends Fragment {
                 .addTestDevice("F2998E6F789E2E4227461CB9139AB42C")
                 .build();
         mAdView.loadAd(adRequest);
+        mInterstitialAd = new InterstitialAd(getActivity());
+        mInterstitialAd.setAdUnitId("ca-app-pub-1763493570283349/2142898915");
+        requestNewInterstitial();
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+                goJokerActivity();
+            }
+        });
 
         return root;
     }
 
     @OnClick(R.id.btn_tell_joke)
     void tellJoke() {
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            goJokerActivity();
+        }
+    }
+
+    private void goJokerActivity() {
+        setLoading(true);
         final Intent intent = new Intent(getActivity(), JokerActivity.class);
         new JokeAsyncTask() {
             @Override
             protected void onPostExecute(String joke) {
+                setLoading(false);
                 intent.putExtra(JOKE_EXTRA_STRING, joke);
                 startActivity(intent);
             }
         }.execute();
+    }
+
+
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("F2998E6F789E2E4227461CB9139AB42C")
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
+    }
+
+    void setLoading(boolean isLoading) {
+        if (isLoading) {
+            btnTellJoke.setEnabled(false);
+            progress.setVisibility(View.VISIBLE);
+        } else {
+            btnTellJoke.setEnabled(true);
+            progress.setVisibility(View.GONE);
+        }
     }
 }
--- app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainFragment.java
@@ -6,11 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 import com.mohamedibrahim.displayjokerlib.JokerActivity;
 import com.udacity.gradle.builditbigger.JokeAsyncTask;
 import com.udacity.gradle.builditbigger.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -21,6 +24,10 @@ import static com.mohamedibrahim.displayjokerlib.JokerActivity.JOKE_EXTRA_STRING
  */
 public class MainFragment extends Fragment {
 
+    @BindView(R.id.progress)
+    ProgressBar progress;
+    @BindView(R.id.btn_tell_joke)
+    Button btnTellJoke;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,13 +39,26 @@ public class MainFragment extends Fragment {
 
     @OnClick(R.id.btn_tell_joke)
     void tellJoke() {
+        setLoading(true);
         final Intent intent = new Intent(getActivity(), JokerActivity.class);
         new JokeAsyncTask() {
             @Override
             protected void onPostExecute(String joke) {
+                setLoading(false);
                 intent.putExtra(JOKE_EXTRA_STRING, joke);
                 startActivity(intent);
             }
         }.execute();
+    }
+
+
+    void setLoading(boolean isLoading) {
+        if (isLoading) {
+            btnTellJoke.setEnabled(false);
+            progress.setVisibility(View.VISIBLE);
+        } else {
+            btnTellJoke.setEnabled(true);
+            progress.setVisibility(View.GONE);
+        }
     }
 }
